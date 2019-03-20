cddaa7284fb56bd5e261eb52b25ae2a765ef8d9f
srinivasrao452 <30620328+srinivasrao452@users.noreply.github.com>
2019-03-20 13:55:06 +0530

    add_Model-commit

--- My_App/models.py
+++ My_App/models.py
@@ -5,6 +5,3 @@ from django.db import models
 class Emp(models.Model):
     eno = models.IntegerField(primary_key=True)
     ename = models.CharField(max_length=100)
-
-    esal = models.DecimalField(max_digits=10,decimal_places=2)
-    
--- My_App/views.py
+++ My_App/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import View
+
+from My_App.models import Emp
 
 # Create your views here.
+class MyView(View):
+    pass
